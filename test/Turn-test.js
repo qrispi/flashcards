be2b2f223a63eaa8303a85b7d3704b96d657719a
Christopher Baum <christopherabaum@gmail.com>
2023-01-30 19:33:56 -0600

    Add Turn-test.js file and test skeleton

---
+++ test/Turn-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  
+});
