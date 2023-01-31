2af96e24d794bc8174c39de9571e270eb872730d
Christopher Baum <christopherabaum@gmail.com>
2023-01-30 20:10:07 -0600

    Instantiate card and turn variables before each test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,13 +1,25 @@
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const Turn = require('../src/Turn');
+const Turn = require('../src/Turn')
+const Card = require('../src/Card')
 
 describe('Turn', function() {
+    let card
+    let turn
 
-  it('should be a function', function() {
-    expect(Turn).to.be.a('function');
-  });
+    beforeEach(function () {
+        card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+        turn = new Turn('pug', card)
+        // console.log(card)
+        // console.log(turn)
+    })
 
-  
-});
+    it('should be a function', () =>
+        expect(Turn).to.be.a('function')
+    )
+
+    it('should be an instance of Turn', () => 
+        expect(turn).to.be.a.instanceOf(Turn)  
+    )
+})
