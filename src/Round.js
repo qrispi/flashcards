0892afd4370ed5f77d11424d7e770da496172ef4
Christopher Baum <christopherabaum@gmail.com>
2023-01-31 22:20:48 -0600

    Add calculatePercentCorrect method

--- src/Round.js
+++ src/Round.js
@@ -21,6 +21,19 @@ class Round {
         this.currentCard = this.currentDeck.cards[this.turns]
         return currentTurn.giveFeedback()
     }
+
+    calculatePercentCorrect() {
+        if (this.turns < 1) {
+            return 0
+        }
+        if (this.turns > 0 && this.incorrectGuesses.length < 1) {
+            return 100
+        }
+        if (this.turns > 0 && this.incorrectGuesses.length === this.turns) {
+            return 0
+        }
+        return Math.round((1 - (this.incorrectGuesses.length / this.turns)) * 100)
+    }
 }
 
 module.exports = Round
