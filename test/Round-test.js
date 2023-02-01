a90586ef4d1637e2fdadac20dd83e50e2788d816
Christopher Baum <christopherabaum@gmail.com>
2023-01-31 20:36:27 -0600

    Add variables for test

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,7 +7,19 @@ const Turn = require('../src/Turn')
 const Round = require('../src/Round')
 
 describe('Round', () => {
-
+    let card1
+    let card2
+    let card3
+    let deck
+    let turn
+    let round
     
-
+    beforeEach(() => {
+        card1 = new Card(13, 'Who is the best Superhero?', ['Spider-Man', 'Batman', 'Superman'], 'Batman')
+        card2 = new Card(27, 'Who is the best Supervillain?', ['Joker', 'Thanos', 'Darth Vader'], 'Joker')
+        card3 = new Card(007, 'Who is the best Spy?', ['James Bond', 'Black Widow', 'Jason Bourne'], 'Black Widow')
+        deck = new Deck([card1, card2, card3])
+        turn = new Turn('Spider-Man', card1)
+        round = new Round()
+    })
 })
