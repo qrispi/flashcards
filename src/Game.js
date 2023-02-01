1729d0a3fec74698290be23f516b534e7afae2bc
Christopher Baum <christopherabaum@gmail.com>
2023-02-01 16:40:35 -0600

    Normalize indentation

--- src/Game.js
+++ src/Game.js
@@ -6,28 +6,28 @@ const Deck = require('../src/Deck')
 const Round = require('../src/Round')
 
 class Game {
-  constructor() {
-    this.currentRound
-  }
+	constructor() {
+		this.currentRound
+	}
 
-  start() {
-    let cards = []
-    prototypeQuestions.forEach(obj => cards.push(new Card(obj["id"], obj["question"], obj["answers"], obj["correctAnswer"])))
-    let deck = new Deck(cards)
-    let round = new Round(deck)
-    this.currentRound = round
-    this.printMessage(deck)
-    this.printQuestion(round)
-  }
+	start() {
+		let cards = []
+		prototypeQuestions.forEach(obj => cards.push(new Card(obj["id"], obj["question"], obj["answers"], obj["correctAnswer"])))
+		let deck = new Deck(cards)
+		let round = new Round(deck)
+		this.currentRound = round
+		this.printMessage(deck)
+		this.printQuestion(round)
+	}
 
-  printMessage(deck) {
-    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+	printMessage(deck) {
+		console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
-  }
+	}
 
-  printQuestion(round) {
-      util.main(round)
-  }
+	printQuestion(round) {
+			util.main(round)
+	}
 }
 
 module.exports = Game
