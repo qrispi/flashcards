3967e6464f4cd1bdf5b4caa292f81e1fef974ba0
Christopher Baum <christopherabaum@gmail.com>
2023-01-31 21:03:59 -0600

    Add returnCurrentCard method and two contructor properties

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,15 @@
+const Turn = require('../src/Turn')
+
 class Round {
     constructor(newDeck) {
         this.currentDeck = newDeck
         this.currentCard = newDeck.cards[0]
+        this.turns = 0
+        this.incorrectGuesses = []
+    }
+
+    returnCurrentCard() {
+        return this.currentCard
     }
 }
 
