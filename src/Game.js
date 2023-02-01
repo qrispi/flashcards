4574a2bae5bbf0dd7293ecae9a2f6153cad7e16d
Christopher Baum <christopherabaum@gmail.com>
2023-01-31 23:08:14 -0600

    Add start method and one property to constructor

--- src/Game.js
+++ src/Game.js
@@ -7,7 +7,17 @@ const Turn = require('../src/Turn')
 const Round = require('../src/Round')
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound
+  }
+
+  start() {
+    let cards = []
+    prototypeQuestions.forEach(obj => cards.push(new Card(obj["id"], obj["question"], obj["answers"], obj["correctAnswer"])))
+    let deck = new Deck(cards)
+    let round = new Round(deck)
+    this.currentRound = round
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
