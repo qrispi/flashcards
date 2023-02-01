1038cb6714c28f6549c63ee8917091b9aadee4db
Christopher Baum <christopherabaum@gmail.com>
2023-02-01 14:47:00 -0600

    Adds print methods to start method and removes round parameter from printMessage method

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,6 @@ const prototypeQuestions = data.prototypeData
 const util = require('./util')
 const Card = require('../src/Card')
 const Deck = require('../src/Deck')
-const Turn = require('../src/Turn')
 const Round = require('../src/Round')
 
 class Game {
@@ -17,9 +16,11 @@ class Game {
     let deck = new Deck(cards)
     let round = new Round(deck)
     this.currentRound = round
+    this.printMessage(deck)
+    this.printQuestion(round)
   }
 
-  printMessage(deck, round) {
+  printMessage(deck) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
