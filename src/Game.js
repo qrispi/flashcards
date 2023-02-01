6454035f0b637a261b591ea39081d16a5e209f2e
Christopher Baum <christopherabaum@gmail.com>
2023-01-31 22:55:08 -0600

    Connect all previous classes to Game and remove semi-colons

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,10 @@
-const data = require('./data');
-const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const data = require('./data')
+const prototypeQuestions = data.prototypeData
+const util = require('./util')
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
+const Turn = require('../src/Turn')
+const Round = require('../src/Round')
 
 class Game {
   constructor() {}
@@ -11,8 +15,8 @@ class Game {
   }
 
   printQuestion(round) {
-      util.main(round);
+      util.main(round)
   }
 }
 
-module.exports = Game;
+module.exports = Game
