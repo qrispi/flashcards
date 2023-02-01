4a9ce1ab715c44129296e5e44d38d7f4136e25fa
Christopher Baum <christopherabaum@gmail.com>
2023-02-01 16:19:53 -0600

    Convert Card tests to arrow functions

--- test/Card-test.js
+++ test/Card-test.js
@@ -3,31 +3,30 @@ const expect = chai.expect
 
 const Card = require('../src/Card')
 
-describe('Card', function () {
+describe('Card', () => {
 	let card
 
 	beforeEach(() => 
 		card = new Card(13, 'Who is the best Superhero?', ['Spider-Man', 'Batman', 'Superman'], 'Batman')
 	)
 
-
-	it('should be a function', function () {
+	it('should be a function', () =>
 		expect(Card).to.be.a('function')
-	})
+	)
 
-	it('should be an instance of Card', function () {
+	it('should be an instance of Card', () =>
 		expect(card).to.be.an.instanceof(Card)
-	})
+	)
 
-	it('should store a question', function () {
+	it('should store a question', () =>
 		expect(card.question).to.equal('Who is the best Superhero?')
-	})
+	)
 
-	it('should store a list of possible answers', function () {
+	it('should store a list of possible answers', () =>
 		expect(card.answers).to.deep.equal(['Spider-Man', 'Batman', 'Superman'])
-	})
+	)
 
-	it('should store the correct answer', function () {
+	it('should store the correct answer', () =>
 		expect(card.correctAnswer).to.equal('Batman')
-	})
+	)
 })
